0deefb2a6c9cc1c8131da3669fb138838a2b6f43
wandile <dnjswo6253@naver.com>
2024-07-27 10:34:52 +0900

    feat: 도메인 모델 정의

--- PocaApp/asgi.py
+++ PocaApp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for PocaAssignment project.
+ASGI config for PocaApp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PocaAssignment.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PocaApp.settings")
 
 application = get_asgi_application()

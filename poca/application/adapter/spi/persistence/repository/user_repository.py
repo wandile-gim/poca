b29deeac745517176f1ce1df844ce62a0ea06cac
wandile <dnjswo6253@naver.com>
2024-07-28 23:20:12 +0900

    fix: 유저 조회 Repository params 수정

--- poca/application/adapter/spi/persistence/repository/user_repository.py
+++ poca/application/adapter/spi/persistence/repository/user_repository.py
@@ -1,6 +1,5 @@
 import decimal
 import logging
-import uuid
 
 from poca.application.adapter.spi.persistence.entity.user import User
 from poca.application.domain.model.user import UserDomain
@@ -17,20 +16,20 @@ class UserRepository(
     def get_user_by_user_email(self, email: str) -> UserDomain:
         try:
             user = User.objects.get(email=email)
-            return UserDomain(user.email, user.password, user.user_id, active=user.active)
+            return UserDomain(user.user_email, user.password, user.id, active=user.active)
         except User.DoesNotExist:
             return None
 
     def get_user_by_user_id(self, user_id: int) -> UserDomain:
         try:
-            user = User.objects.get(user_id=user_id)
-            return UserDomain(user.email, user.password, user.user_id, active=user.active)
+            user = User.objects.get(id=user_id)
+            return UserDomain(email=user.user_email, user_id=user.id, active=user.is_active, balance=user.balance)
         except User.DoesNotExist:
             return None
 
     def save_user_balance(self, user_id: int, balance: decimal.Decimal) -> bool:
         try:
-            user = User.objects.get(user_id=user_id)
+            user = User.objects.get(id=user_id)
             user.balance = balance
             user.save()
         except Exception as e:
